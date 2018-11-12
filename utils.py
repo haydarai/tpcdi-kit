e7128ee701a750b72d2515650729e4b695e57967
Unknown <dwiprasetyo.an@gmail.com>
2018-11-12 12:12:46 +0100

    Add row-to-list transformer as parameter

--- utils.py
+++ utils.py
@@ -7,17 +7,27 @@ from contextlib import ExitStack
 from heapq import merge
 
 class CSV_Transformer():
-    @staticmethod
-    def transofrm(line,delimiter):
-        return line.split(delimiter)
-    
-    @staticmethod
-    def inverse_transform(transformed, delimiter):
-        return delimiter.join(transformed)
-
-def external_sort(input_file, col_idx, max_chunk_size, onFinished):
     """
+    Transform a row in a csv flat file to a list
+    Attributes:
+        delimiter (str): Character used to limit a field entries to other fields.
+    """
+    def __init__(self, delimiter):
+        self.delimiter = delimiter
+    def transofrm(self, line):
+        return line.split(self.delimiter)
+    def inverse_transform(self, transformed):
+        return self.delimiter.join(transformed)
 
+def external_sort(input_file, transformer, col_idx, max_chunk_size, on_finished):
+    """
+    Sort file based on col_idx outside main memory. 
+    Args:
+        input_file (str): Path to the input file.
+        transformer (obj): Instance of a row-to-list transformer class.
+        col_idx (int): Index of the list column used for sorting.
+        max_chunk_size (int): Maximum number of lines contained in a chunk file.
+        on_finished (fun): Function to be executed with the outputfile as parameter when the sorting is finished.
     """
     
     chunk = []
@@ -27,29 +37,30 @@ def external_sort(input_file, col_idx, max_chunk_size, onFinished):
         # Split input to multiple files
         for line in f:
             if len(chunk)<max_chunk_size:
-                chunk.append(CSV_Transformer.transofrm(line, "|"))
+                chunk.append(transformer.transofrm(line))
             else:
                 chunk_name = 'chunk_{}.chk'.format(counter)
                 chunk = sorted(chunk, key=lambda row: row[col_idx], reverse=False)
                 chunk_names.append(os.path.join(tmpdirname, chunk_name))
                 with open(os.path.join(tmpdirname, chunk_name), 'w') as split_file:
                     for line in chunk:
-                        split_file.write(CSV_Transformer.inverse_transform(line, "|"))
+                        split_file.write(transformer.inverse_transform(line))
                 counter +=1
                 chunk = []
         # Merge        
         # Credit: https://stackoverflow.com/questions/23450145/sort-a-big-file-with-python-heapq-merge
         output_path = os.path.join(tmpdirname, input_file.split('/')[-1])
-        with ExitStack() as stack, open("output_path", 'w') as output_file:
+        with ExitStack() as stack, open(output_path, 'w') as output_file:
             files = [stack.enter_context(open(chunk)) for chunk in chunk_names]
-            output_file.writelines(merge(*files, key=lambda row: CSV_Transformer.transofrm(row, "|",)[col_idx]))
+            output_file.writelines(merge(*files, key=lambda row: transformer.transofrm(row)[col_idx]))
 
             # Do something with the sorted file
-            onFinished(output_path)
+            on_finished(output_path)
 
 def dummyOnFinish(sorted_file):
     pass
 
 # Usage Example
 if __name__ == "__main__":
-    external_sort(input_file='staging/5/Batch1/Industry.txt',col_idx=2, max_chunk_size=2, onFinished=dummyOnFinish)
+    pipe_delimited_transformer = CSV_Transformer(delimiter="|")
+    external_sort(input_file='staging/5/Batch1/Industry.txt',transformer = pipe_delimited_transformer, col_idx=1, max_chunk_size=2, on_finished=dummyOnFinish)
