b2ae9c0ac03d00e161073f05b82b461c598ccb50
Haydar Ali Ismail <haydar_ai@yahoo.com>
2018-11-16 20:25:54 +0100

    Fix duplicated line of code

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     # Parse user's option
     parser = optparse.OptionParser()
     parser.add_option("-s", "--scalefactor", help="Scale factor used")
-    parser.add_option("-d", "--dbname", help="Name of database schema to which the data will be loaded")
+    parser.add_option(
+        "-d", "--dbname", help="Name of database schema to which the data will be loaded")
 
     (options, args) = parser.parse_args()
 
@@ -20,25 +21,20 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('db.conf')
 
-  # Instantiate TPCDI_Loader and execute the loader in order
-  loader = TPCDI_Loader(options.scalefactor, options.dbname, config)
-  loader.load_dimDate()
-  loader.load_dimTime()
-  loader.load_statusType()
-  loader.load_taxRate()
-  loader.load_tradeType()
-  loader.load_audit()
     # Instantiate TPCDI_Loader and execute the loader in order
     loader = TPCDI_Loader(options.scalefactor, options.dbname, config)
+    loader.load_dimDate()
+    loader.load_dimTime()
     loader.load_statusType()
     loader.load_taxRate()
     loader.load_tradeType()
     loader.load_audit()
-    
+
     # Stupid example, joining the same Industry csv file
     # It will return iterator
     pipe_delimited_transformer = CSV_Transformer(delimiter="|")
-    res = sort_merge_join('staging/5/Batch1/Industry.txt','staging/5/Batch1/Industry.txt',0,0,pipe_delimited_transformer,pipe_delimited_transformer)
+    res = sort_merge_join('staging/5/Batch1/Industry.txt', 'staging/5/Batch1/Industry.txt',
+                        0, 0, pipe_delimited_transformer, pipe_delimited_transformer)
 
     # Use next() to get the next result
     print(next(res))
