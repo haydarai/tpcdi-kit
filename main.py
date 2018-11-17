973049e351380b5153c477002cd755d39827dcaf
Unknown <dwiprasetyo.an@gmail.com>
2018-11-17 23:50:37 +0100

    Add Security Staging Table Loading

--- main.py
+++ main.py
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     loader.load_staging_taxRate()
     loader.load_staging_tradeType()
     loader.load_audit()
-    loader.load_staging_company()
+    loader.load_staging_finwire()
     
     loader.load_target_dim_company()
