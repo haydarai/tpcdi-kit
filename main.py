3588c018cb504efe6b5f0fa03c5481f88bb5ffbe
Unknown <dwiprasetyo.an@gmail.com>
2018-11-18 17:28:40 +0100

    Add dimSecurity data loading
    - Add dimSecurity
    - Modify surrogate key to use autoincrement

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     loader.init_dimCustomer()
     loader.load_staging_company()
     loader.load_staging_finwire()
-    
     loader.load_target_dim_company()
     loader.load_target_financial()
+    loader.load_target_dim_security()
